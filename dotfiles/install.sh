3a8f14ac302f70b85857b410ae0951ca06410fa3
Matthew Elledge <131494346+mthyuu@users.noreply.github.com>
2025-10-20 09:18:20 -0400

    Add ripgrep to installation script

--- dotfiles/install.sh
+++ dotfiles/install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-sudo pacman -S --noconfirm --needed gimp tldr go fzf unzip
+sudo pacman -S --noconfirm --needed gimp tldr go fzf unzip ripgrep
 sudo pacman -Syu
 sudo pacman -S hyprland hyprlock hyprpaper --noconfirm --needed
 sudo pacman -S tff-font-awesome ttf-nerd-font-symbols ttf-jetbrains-mono-nerd inter-font adobe-source-code-pro-fonts
